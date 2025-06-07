bff48c955588d7e0dbae5ce29442cfae72dad3a5
Rachan Murthy <rachan3thi@gmail.com>
2025-06-07 09:47:47 +0530

    Remove Twitter trends functionality and related imports from the API, including views and service files.

--- ElizaServices/elizaservices/elizaservicesapi/urls.py
+++ ElizaServices/elizaservices/elizaservicesapi/urls.py
@@ -7,11 +7,9 @@ from .views import (
     LatestTweetView,
     SingleTweetView,
     ValidSendersView,
-    TwitterTrendsView,
     EnvironmentalNewsView,
     ImageGenView,
     PersonalityAnalysisView,
-    TwitterTrendsTimeframesView,
     QueuedTweetsView,
     QueuedTweetDetailView
 )
@@ -20,15 +18,18 @@ urlpatterns = [
     path('popularity/', PopularityScoreView.as_view(), name='popularity-score'),
     path('safety/', SafetyScoreView.as_view(), name='safety-score'),
     path('compare_hashtags/', CompareHashtagsView.as_view(), name='compare-hashtags'),
+
     path('tweets/', TweetsView.as_view(), name='tweets'),
     path('tweets/latest/', LatestTweetView.as_view(), name='latest-tweet'),
     path('tweets/senders/', ValidSendersView.as_view(), name='valid-senders'),
     path('tweets/<str:tweet_id>/', SingleTweetView.as_view(), name='tweet-detail'),
-    path('twitter/trends/', TwitterTrendsView.as_view(), name='twitter-trends'),
+
     path('news/', EnvironmentalNewsView.as_view(), name='environmental-news'),
+
     path('imagegen/', ImageGenView.as_view(), name='imagegen'),
+
     path('personality-analysis/', PersonalityAnalysisView.as_view(), name='personality-analysis'),
-    path('twitter-trends/timeframes/', TwitterTrendsTimeframesView.as_view(), name='twitter-trends-timeframes'),
+
     path('queued-tweets/', QueuedTweetsView.as_view(), name='queued-tweets'),
-    path('queued-tweets/<int:pk>/', QueuedTweetDetailView.as_view(), name='queued-tweet-detail'),
+    path('queued-tweets/<int:pk>/', QueuedTweetDetailView.as_view(), name='queued-tweet-detail')
 ]
